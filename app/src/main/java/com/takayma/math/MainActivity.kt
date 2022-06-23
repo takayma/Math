80f9a9c6901f474eb9cd3fbe6390122026344eb1
takayma <imtakabaka@gmail.com>
2022-06-23 15:12:19 +0400

    commit

--- app/src/main/java/com/takayma/math/MainActivity.kt
+++ app/src/main/java/com/takayma/math/MainActivity.kt
@@ -2,11 +2,9 @@ package com.takayma.math
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
 
 
 class  MainActivity : AppCompatActivity() {
